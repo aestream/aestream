c3f4969469c60b92ebfdb6ffbb99d09b045bec89
Jens E. Pedersen <jensegholm@protonmail.com>
2021-01-21 22:50:37 +0100

    Christian magic

--- convert.cpp
+++ convert.cpp
@@ -57,32 +57,45 @@ convert_polarity_events(std::vector<AEDAT::PolarityEvent> &polarity_events,
   return events.clone();
 }
 
-torch::Tensor event_tensor(std::vector<AEDAT::PolarityEvent> &polarity_events, 
-                                    torch::IntArrayRef image_dimensions) {
-  const size_t length = polarity_events.size();
-  auto size_vector = image_dimensions.vec();
-  size_vector.insert(size_vector.begin(), length);
-  const torch::IntArrayRef size = torch::IntArrayRef(size_vector);
-  torch::Tensor tensor = torch::empty(size);
-  for (size_t idx = 0; idx < length; idx++) {
-    auto event = polarity_events[idx];
-    tensor[idx][event.x][event.y] = event.polarity ? 1 : -1;
-  }
-  return tensor;
-}
-
-std::vector<torch::Tensor> convert_polarity(std::vector<AEDAT::PolarityEvent> &polarity_events,
+std::vector<torch::Tensor>
+convert_polarity(std::vector<AEDAT::PolarityEvent> &polarity_events,
                  const int64_t window_size,
                  const int64_t window_step,
-                 const std::vector<int64_t> &image_dimension) {
-  auto tensor = event_tensor(polarity_events, image_dimension);
-  const size_t size = polarity_events.size();
-  std::vector<torch::Tensor> values;
-  for (size_t idx = 0; idx < (size - window_size); idx += window_step) {
-    torch::Tensor t = tensor.slice(0, idx, idx + window_size).clone();
-    values.push_back(t);
+                 const std::vector<int64_t> &image_dimensions) {
+  std::vector<torch::Tensor> event_tensors;
+  size_t start = 0;
+  size_t idx = 0;
+  auto last_event = polarity_events.back();
+  while (start <  last_event.timestamp - window_size) {
+    auto event = polarity_events[idx];
+    size_t start_time = event.timestamp;
+    std::vector<int64_t> indices;
+    std::vector<int8_t> values;
+
+    while (event.timestamp < start + window_size) {
+        indices.push_back(event.timestamp - start_time);
+        indices.push_back(event.x);
+        indices.push_back(event.y);
+        values.push_back(event.polarity ? 1 : -1);
+        idx += 1;
+        event = polarity_events[idx];
+    }
+
+    // create sparse tensor
+    auto index_options = torch::TensorOptions().dtype(torch::kInt64);
+    torch::Tensor ind = torch::from_blob(
+      indices.data(), {static_cast<uint32_t>(indices.size() / 3), 3}, index_options).permute({1,0});
+
+    auto value_options = torch::TensorOptions().dtype(torch::kInt8);
+    torch::Tensor val = torch::from_blob(
+      values.data(), {static_cast<uint32_t>(indices.size()/3)}, value_options);
+    auto events = torch::sparse_coo_tensor(ind, val, {window_size, image_dimensions[0], image_dimensions[1]});
+
+    event_tensors.push_back(events.clone());
+    start += window_size;
   }
-  return values;
+
+  return event_tensors;
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
