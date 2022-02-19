5a1faa510dc3eeeb3272c234f3fa15209887cf1f
jegp <jens@jepedersen.dk>
2022-02-20 00:04:18 +0100

    Added nix build

--- flake.nix
+++ flake.nix
@@ -9,7 +9,10 @@
       let
         pkgs = import nixpkgs { inherit system; };
         py = pkgs.python39Packages;
+<<<<<<< HEAD
         python-requirements = builtins.readFile ./requirements.txt;
+=======
+>>>>>>> 63aaeb5 (Added nix build)
         aestream = pkgs.stdenv.mkDerivation {
           name = "aestream";
           version = "0.1.0";
