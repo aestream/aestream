924cc32e7461be9bef3738024bf25bbc75cad38d
jegp <jens@jepedersen.dk>
2022-05-06 19:37:21 +0200

    Updated nix flakes

--- flake.nix
+++ flake.nix
@@ -10,6 +10,17 @@
         pkgs = import nixpkgs { inherit system; };
         py = pkgs.python39Packages;
         python-requirements = builtins.readFile ./requirements.txt;
+        libcaer = pkgs.stdenv.mkDerivation {
+          pname = "libcaer";
+          version = "1.0";
+          src = pkgs.fetchurl {
+            url = https://gitlab.com/inivation/dv/libcaer/-/archive/54191a3b27db4645ae3f83d96cf9bad5e1d646da/libcaer-54191a3b27db4645ae3f83d96cf9bad5e1d646da.tar.bz2;
+            sha1 = "3b531ddda80513e169227b2a77823ad64f0aedfb";
+          };
+          nativeBuildInputs = with pkgs; [
+            pkg-config libusb1 cmake gcc flatbuffers ninja lz4
+          ];
+        };
         aestream = pkgs.stdenv.mkDerivation {
           name = "aestream";
           version = "0.1.0";
@@ -22,6 +33,8 @@
             pkgs.flatbuffers
             pkgs.ninja
             pkgs.lz4
+            pkgs.libtorch-bin
+            libcaer
           ];
           configurePhase = "cmake -GNinja -Bbuild/ .";
           buildPhase = "ninja -C build";
@@ -38,6 +51,7 @@
           src = ./.;
           requirements = python-requirements;
 
+          buildInputs = [ libcaer ];
           nativeBuildInputs = [
             pkgs.which
           ];
