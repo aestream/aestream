be9b489dec6f0f5d77c0dd4b79b0a84498561d04
Jens E. Pedersen <jensegholm@protonmail.com>
2022-06-30 12:53:33 +0200

    Removed aarch64 builds for now

--- .github/deps/manylinux_dependencies.sh
+++ .github/deps/manylinux_dependencies.sh
@@ -4,9 +4,6 @@ yum install -y zlib-devel ninja-build libgusb-devel
 cd /
 curl -L https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip > libtorch.zip
 unzip libtorch.zip
-# export CMAKE_PREFIX_PATH=/libtorch:/libtorch/lib:${CMAKE_PREFIX_PATH}
-# export LD_LIBRARY_PATH=/libtorch:/libtorch/lib:${LD_LIBRARY_PATH}
-# export PATH=/libtorch/:/libtorch/lib:${PATH}
 
 # Install libcaer
 cd /
