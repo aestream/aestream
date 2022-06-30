bc2b68e37e02a2b57a91c9035d806e80f0b63898
Jens E. Pedersen <jensegholm@protonmail.com>
2022-06-30 16:48:45 +0200

    Resolved pytorch path

--- .github/deps/manylinux_dependencies.sh
+++ .github/deps/manylinux_dependencies.sh
@@ -1,15 +1,18 @@
 # System dependencies
-yum install -y zlib-devel ninja-build libgusb-devel wget
+yum install -y zlib-devel ninja-build libgusb-devel
 
-cd / # Ensure root directory
+echo `pwd`
+
+# Ensure we're in the root
+cd /root
 
 # Install libtorch
-wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip -O libtorch.zip
-unzip libtorch.zip
+curl -L -s -m 100 https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip > libtorch.zip
+ls -alh .
+unzip -q libtorch.zip
 
 # Install libcaer
-wget https://gitlab.com/inivation/dv/libcaer/-/archive/3.3.14/libcaer-3.3.14.tar.gz -O libcaer.tar.gz
-tar xf libcaer.tar.gz
+curl -L -s -m 100 https://gitlab.com/inivation/dv/libcaer/-/archive/3.3.14/libcaer-3.3.14.tar.gz | tar zxf -
 cd libcaer-3.3.14
 cmake -DCMAKE_INSTALL_PREFIX=/usr .
 make -j 4 && make install
