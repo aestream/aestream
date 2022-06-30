05ab76290d8660dd52986a3d5c591f8d3f2d1222
Jens E. Pedersen <jensegholm@protonmail.com>
2022-06-30 15:07:07 +0200

    Fixed wget downloads

--- .github/deps/manylinux_dependencies.sh
+++ .github/deps/manylinux_dependencies.sh
@@ -1,14 +1,13 @@
 yum install -y zlib-devel ninja-build libgusb-devel wget
 
 # Install libtorch
-cd /
-wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip -O libtorch.zip
 unzip libtorch.zip
 
 # Install libcaer
-cd /
-wget https://gitlab.com/inivation/dv/libcaer/-/archive/3.3.14/libcaer-3.3.14.tar.gz 
-tar xf libcaer-3.3.14.tar.gz
+wget https://gitlab.com/inivation/dv/libcaer/-/archive/3.3.14/libcaer-3.3.14.tar.gz -O libcaer.tar.gz
+tar xf libcaer.tar.gz
 cd libcaer-3.3.14
 cmake -DCMAKE_INSTALL_PREFIX=/usr .
 make -j 4 && make install
+cd ..
