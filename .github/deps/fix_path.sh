3d840b5c9551760a08ab61d443b1097a10a14e70
Jens E. Pedersen <jensegholm@protonmail.com>
2022-10-25 10:14:44 +0200

    Cleaned build repo between python version builds

--- .github/deps/fix_path.sh
+++ .github/deps/fix_path.sh
@@ -3,6 +3,4 @@
 pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
 TORCH_PATH=$(python -c "import os,torch; print(os.path.dirname(torch.__file__) + \"/lib/\")")
 AESTREAM_PATH=$(readlink -f $(dirname $(find _skbuild -name "libaedat4.so" | grep setuptools)))
-
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${TORCH_PATH}:${AESTREAM_PATH}"
-echo $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="${TORCH_PATH}:${AESTREAM_PATH}"
