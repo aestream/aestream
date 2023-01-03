13a52f0f4ebb2c4f5282672ded17322fe29522f9
Jens E. Pedersen <jensegholm@protonmail.com>
2023-01-03 20:56:41 +0100

    Added DAT file reading

--- src/input/prophesee.cpp
+++ src/input/prophesee.cpp
@@ -2,8 +2,8 @@
 
 // event generator for Prophesee cameras
 Generator<AEDAT::PolarityEvent> prophesee_event_generator(
-    const std::optional<std::string> serial_number,
-                          const std::atomic<bool> &runFlag) {
+    const std::atomic<bool> &runFlag,
+    const std::optional<std::string> serial_number = std::nullopt) {
 
   Metavision::Camera cam;
 
@@ -16,25 +16,22 @@ Generator<AEDAT::PolarityEvent> prophesee_event_generator(
     }
   } catch (const std::exception &e) {
     if (serial_number.has_value()) {
-      std::cout << "Failure with serial number '" << serial_number.value()
+      std::cerr << "Failure with serial number '" << serial_number.value()
                 << "': " << e.what() << std::endl;
     } else {
-      std::cout << "Failure to identify Prophesee camera: " << e.what() << std::endl;
+      std::cerr << "Failure to identify Prophesee camera: " << e.what()
+                << std::endl;
     }
-
-    std::cout << "Available cameras: " << std::endl;
 
     Metavision::AvailableSourcesList available_systems =
         cam.list_online_sources();
 
     for (int i = 0;
          i < available_systems[Metavision::OnlineSourceType::USB].size(); i++) {
-      std::cout << "- "
+      std::cerr << "- "
                 << available_systems[Metavision::OnlineSourceType::USB][i]
                 << std::endl;
     }
-
-    std::cout << std::endl;
 
     throw std::invalid_argument(
         "Please choose one of the above listed serial numbers and run again!");
@@ -55,7 +52,6 @@ Generator<AEDAT::PolarityEvent> prophesee_event_generator(
   cam.start();
 
   // keep running while camera is on or video is finished
-  try {
   while (cam.is_running() && runFlag.load()) {
     if ((ev_start != NULL) && (ev_final != NULL)) {
       // iterate over events in buffer and convert to AEDAT Polarity Event
@@ -69,9 +65,7 @@ Generator<AEDAT::PolarityEvent> prophesee_event_generator(
       ev_final = NULL;
     }
   }
-  } catch (const std::exception &e) {
-    std::cerr << "Unexpected exception occurred: " << e.what();
-  }
+
   // if video is finished, stop camera - will never get here with live camera
   cam.stop();
 }
