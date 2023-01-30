6f173c94465f0d9f40f293f97c265a9c9b775185
Jens E. Pedersen <jensegholm@protonmail.com>
2023-01-30 16:37:03 +0100

    Uncommented torch installation

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ import os
 
 from skbuild import setup  # Use scikit-build
 
-# import torch
-# from torch.utils import cpp_extension
+try:
+    import torch
+    from torch.utils import cpp_extension
+except ImportError:
+    pass
 
 pwd = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(pwd, "README.md"), encoding="utf-8") as fp:
@@ -12,23 +15,23 @@ with open(os.path.join(pwd, "README.md"), encoding="utf-8") as fp:
 # C++ config
 cmake_args = [
     "-DWITH_PYTHON=1",
-    # f"-DCMAKE_PREFIX_PATH='{os.path.dirname(torch.__file__)};{torch.utils.cmake_prefix_path}'",
-    # f"-DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI={1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0} -fPIC'",
+    f"-DCMAKE_PREFIX_PATH='{os.path.dirname(torch.__file__)};{torch.utils.cmake_prefix_path}'",
+    f"-DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI={1 if torch._C._GLIBCXX_USE_CXX11_ABI else 0} -fPIC'",
 ]
 
 # Define extension based on CUDA availability
-# cuda_home = cpp_extension._find_cuda_home()
-# if cuda_home is not None:
-#     archs = ";".join([x[3:] for x in torch.cuda.get_arch_list()])
-#     flags = " ".join(cpp_extension._get_cuda_arch_flags())
-#     cmake_args += [
-#         #f"-DCMAKE_CUDA_ARCHITECTURES='{archs}'",
-#         "-DCMAKE_CUDA_ARCHITECTURES='50;60;70;75;80;86'",
-#         f'-DCMAKE_CUDA_FLAGS={flags}',
-#         f"-DCMAKE_CUDA_COMPILER={cuda_home}/bin/nvcc",
-#         f"-DCUDA_INCLUDE_DIRS={cuda_home}/include",
-#     ]
-#     print(flags)
+cuda_home = cpp_extension._find_cuda_home()
+if cuda_home is not None:
+    archs = ";".join([x[3:] for x in torch.cuda.get_arch_list()])
+    flags = " ".join(cpp_extension._get_cuda_arch_flags())
+    cmake_args += [
+        f"-DCMAKE_CUDA_ARCHITECTURES='{archs}'",
+        # "-DCMAKE_CUDA_ARCHITECTURES='50;60;70;75;80;86'",
+        f'-DCMAKE_CUDA_FLAGS={flags}',
+        f"-DCMAKE_CUDA_COMPILER={cuda_home}/bin/nvcc",
+        f"-DCUDA_INCLUDE_DIRS={cuda_home}/include",
+    ]
+    print(flags)
 
 # Setuptools entrypoint
 setup(
