bd1d01a2afcdf4b529dbc3188a0dc75ffb009311
Jens E. Pedersen <jensegholm@protonmail.com>
2023-02-15 23:36:07 +0100

    Temporary CUDA pointer fix

--- src/pybind/tensor_buffer.hpp
+++ src/pybind/tensor_buffer.hpp
@@ -11,36 +11,24 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/tensor.h>
 
-#ifdef USE_CUDA
-#include <cuda.h>
-template <typename scalar_t>
-void index_increment_cuda(scalar_t *array, std::vector<int> offsets,
-                          int *event_device_pointer);
+// struct nop
+// {
+//     template <typename T>
+//     void operator() (T const &) const noexcept { }
+// };
 
-template <typename scalar_t> scalar_t *alloc_memory_cuda(size_t buffer_size);
-
-template <typename scalar_t>
-void free_memory_cuda(scalar_t *cuda_device_pointer);
-
-using tensor_t =
-    nb::tensor<nb::pytorch, float, nb::shape<2, nb::any>, nb::device::cuda>;
-#else
-using tensor_t = nb::tensor<nb::numpy, float, nb::shape<2, nb::any>>;
-#endif
+using tensor_t = nb::tensor<float, nb::shape<2, nb::any>>;
+using buffer_t = float *;
 
 class TensorBuffer {
 private:
   const std::vector<int64_t> shape;
   uint64_t current_timestamp = 0;
   const std::string &device;
-#ifdef USE_TORCH
-  torch::TensorOptions options_buffer;
-  torch::TensorOptions options_copy;
-#endif
 
   std::mutex buffer_lock;
-  std::unique_ptr<cache_t> buffer1;
-  std::unique_ptr<cache_t> buffer2;
+  buffer_t buffer1;
+  buffer_t buffer2;
 #ifdef USE_CUDA
   std::vector<int> offset_buffer;
   int *cuda_device_pointer;
--- src/pybind/tensor_iterator.hpp
+++ src/pybind/tensor_iterator.hpp
@@ -21,5 +21,5 @@ public:
   TensorIterator(Generator<AER::Event> &generator, py_size_t shape,
                  size_t time_window);
   template <typename T> void assign_event(T *array, int16_t x, int16_t y);
-  tensor_t next();
+  float * next();
 };
