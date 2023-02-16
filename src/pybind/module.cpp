f482a238d3b73d24dbf4b7d1053ed083965a4ca2
cameron-git <cameronbarkeran@gmail.com>
2023-02-16 00:28:46 +0000

    docs makefile

--- src/pybind/module.cpp
+++ src/pybind/module.cpp
@@ -17,9 +17,7 @@
 namespace nb = nanobind;
 
 NB_MODULE(aestream_ext, m) {
-
-     m.doc("aestream_ext docstring");
-
+     // m.doc() = "aestream_ext docstring";
      //   NB_NUMPY_DTYPE(AER::Event, timestamp, x, y, polarity);
 
      nb::class_<AER::Event>(m, "Event")
@@ -34,73 +32,73 @@ NB_MODULE(aestream_ext, m) {
                                 [](const AER::Event &e)
                                 { return e.polarity; });
 
-         //   nb::class_<Iterator>(m, "EventIterator")
-         //       //  .def( // Thanks to https://stackoverflow.com/a/57217995/999865
-         //       // "__iter__",
-         //       // [](Iterator &f) { return nb::make_iterator(f.begin(), f.end()); },
-         //       // nb::keep_alive<0, 1>() /* Keep object alive while iterator exists
-         //       //                         */
-         //       // ,
-         //       // nb::return_value_policy::move)
-         //       .def("__iter__", [](Iterator &it) -> Iterator & { return it; })
-         //       .def("__next__", &Iterator::next, nb::rv_policy::reference);
+               //   nb::class_<Iterator>(m, "EventIterator")
+               //       //  .def( // Thanks to https://stackoverflow.com/a/57217995/999865
+               //       // "__iter__",
+               //       // [](Iterator &f) { return nb::make_iterator(f.begin(), f.end()); },
+               //       // nb::keep_alive<0, 1>() /* Keep object alive while iterator exists
+               //       //                         */
+               //       // ,
+               //       // nb::return_value_policy::move)
+               //       .def("__iter__", [](Iterator &it) -> Iterator & { return it; })
+               //       .def("__next__", &Iterator::next, nb::rv_policy::reference);
 
-         //   nb::class_<FrameIterator>(m, "FrameIterator")
-         //       .def("__iter__", [](FrameIterator &it) -> FrameIterator & { return
-         //       it; }) .def("__next__", &FrameIterator::next);
+               //   nb::class_<FrameIterator>(m, "FrameIterator")
+               //       .def("__iter__", [](FrameIterator &it) -> FrameIterator & { return
+               //       it; }) .def("__next__", &FrameIterator::next);
 
-         //   nb::class_<PartIterator>(m, "PartIterator")
-         //       .def("__iter__", [](PartIterator &it) -> PartIterator & { return it;
-         //       }) .def("__next__", &PartIterator::next);
+               //   nb::class_<PartIterator>(m, "PartIterator")
+               //       .def("__iter__", [](PartIterator &it) -> PartIterator & { return it;
+               //       }) .def("__next__", &PartIterator::next);
 
-         nb::class_<FileInput>(m, "FileInput")
-             .def(nb::init<std::string, py_size_t, std::string, bool>(),
-                  nb::arg("filename"), nb::arg("shape"), nb::arg("device") = "cpu",
-                  nb::arg("ignore_time") = false)
-             .def("__enter__", &FileInput::start_stream)
-             .def("__exit__", &FileInput::stop_stream, nb::arg("a").none(),
-                  nb::arg("b").none(), nb::arg("c").none())
-             //  .def("events", &FileInput::events)
-             //  .def("frames",
-             //       [](nb::object fobj, size_t n_events_per_part) {
-             //         return FrameIterator(fobj.cast<FileInput &>(),
-             //         n_events_per_part, fobj);
-             //       })
-             //  .def("events_co", &FileInput::events_co)
-             .def("is_streaming", &FileInput::get_is_streaming)
-             .def("start_stream", &FileInput::start_stream)
-             .def("stop_stream", &FileInput::stop_stream)
-             .def("read", &FileInput::read)
-             //  .def("parts",
-             //       [](nb::object fobj, size_t n_events_per_part) {
-             //         //    std::cout << i.filename << std::endl;
-             //         //    if (endsWith(i.filename, "aedat4")) {
-             //         // std::cout << "AEDAT" << std::endl;
-             //         return Iterator(fobj.cast<FileInput &>(), n_events_per_part,
-             //         fobj);
-             //         //    } else {
-             //         // return PartIterator(i, n_events_per_part, fobj);
-             //         //    }
-             //       })
-             //  .def( // Thanks to https://stackoverflow.com/a/57217995/999865
-             //      "__iter__",
-             //      [](FileInput &f) { return
-             //      nb::make_iterator(nb::type<Generator<AER::Event>::Iter>,
-             //      "iterator", f.begin(), f.end()); }, nb::keep_alive<0, 1>() /* Keep
-             //      object alive while iterator exists */)
-             //  .def("__next__", &FileInput::begin)
-             ;
+               nb::class_<FileInput>(m, "FileInput")
+                   .def(nb::init<std::string, py_size_t, std::string, bool>(),
+                        nb::arg("filename"), nb::arg("shape"), nb::arg("device") = "cpu",
+                        nb::arg("ignore_time") = false)
+                   .def("__enter__", &FileInput::start_stream)
+                   .def("__exit__", &FileInput::stop_stream, nb::arg("a").none(),
+                        nb::arg("b").none(), nb::arg("c").none())
+                   //  .def("events", &FileInput::events)
+                   //  .def("frames",
+                   //       [](nb::object fobj, size_t n_events_per_part) {
+                   //         return FrameIterator(fobj.cast<FileInput &>(),
+                   //         n_events_per_part, fobj);
+                   //       })
+                   //  .def("events_co", &FileInput::events_co)
+                   .def("is_streaming", &FileInput::get_is_streaming)
+                   .def("start_stream", &FileInput::start_stream)
+                   .def("stop_stream", &FileInput::stop_stream)
+                   .def("read", &FileInput::read)
+                   //  .def("parts",
+                   //       [](nb::object fobj, size_t n_events_per_part) {
+                   //         //    std::cout << i.filename << std::endl;
+                   //         //    if (endsWith(i.filename, "aedat4")) {
+                   //         // std::cout << "AEDAT" << std::endl;
+                   //         return Iterator(fobj.cast<FileInput &>(), n_events_per_part,
+                   //         fobj);
+                   //         //    } else {
+                   //         // return PartIterator(i, n_events_per_part, fobj);
+                   //         //    }
+                   //       })
+                   //  .def( // Thanks to https://stackoverflow.com/a/57217995/999865
+                   //      "__iter__",
+                   //      [](FileInput &f) { return
+                   //      nb::make_iterator(nb::type<Generator<AER::Event>::Iter>,
+                   //      "iterator", f.begin(), f.end()); }, nb::keep_alive<0, 1>() /* Keep
+                   //      object alive while iterator exists */)
+                   //  .def("__next__", &FileInput::begin)
+                   ;
 
-         nb::class_<UDPInput>(m, "UDPInput")
-             .def(nb::init<py_size_t, std::string, int>(), nb::arg("shape"),
-                  nb::arg("device") = "cpu", nb::arg("port") = 3333)
-             .def("__enter__", &UDPInput::start_stream)
-             .def("__exit__", &UDPInput::stop_stream, nb::arg("a").none(),
-                  nb::arg("b").none(), nb::arg("c").none())
-             .def("start_stream", &UDPInput::start_stream)
-             .def("stop_stream", &UDPInput::stop_stream, nb::arg("a").none(),
-                  nb::arg("b").none(), nb::arg("c").none())
-             .def("read", &UDPInput::read);
+               nb::class_<UDPInput>(m, "UDPInput")
+                   .def(nb::init<py_size_t, std::string, int>(), nb::arg("shape"),
+                        nb::arg("device") = "cpu", nb::arg("port") = 3333)
+                   .def("__enter__", &UDPInput::start_stream)
+                   .def("__exit__", &UDPInput::stop_stream, nb::arg("a").none(),
+                        nb::arg("b").none(), nb::arg("c").none())
+                   .def("start_stream", &UDPInput::start_stream)
+                   .def("stop_stream", &UDPInput::stop_stream, nb::arg("a").none(),
+                        nb::arg("b").none(), nb::arg("c").none())
+                   .def("read", &UDPInput::read);
 
 #ifdef USE_INIVATION
   nb::class_<USBInput>(m, "USBInput")
