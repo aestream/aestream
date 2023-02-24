ba80d900e759c268d39d45bb62a06c056e83e7a2
Jens E. Pedersen <jensegholm@protonmail.com>
2023-02-24 21:09:00 +0100

    Corrected wheel repair path

--- .github/deps/fix_path.sh
+++ .github/deps/fix_path.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
-TORCH_PATH=$(python -c "import os,torch; print(os.path.dirname(torch.__file__) + \"/lib/\")")
-AESTREAM_PATH=$(readlink -f $(dirname $(find _skbuild -name "libaedat4.so" | grep setuptools)))
-export LD_LIBRARY_PATH="${TORCH_PATH}:${AESTREAM_PATH}"
+# pip install torch --extra-index-url https://download.pytorch.org/whl/cpu
+# TORCH_PATH=$(python -c "import os,torch; print(os.path.dirname(torch.__file__) + \"/lib/\")")
+INSTALL_PATH=$(find _skbuild -name "aestream" -type d | grep install)
+AESTREAM_PATH=$(readlink -f $INSTALL_PATH)
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${AESTREAM_PATH}"
