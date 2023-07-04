bbbadc3a9589a023715e0088b2d9a57d3b544585
jegp <jens@jepedersen.dk>
2023-07-04 03:17:03 +0200

    Added ZMQ synsense integration

--- src/input/zmq.cpp
+++ src/input/zmq.cpp
@@ -6,20 +6,36 @@
 
 Generator<AER::Event> open_zmq(const std::string socket, std::atomic<bool>& runFlag) {
   zmq::context_t ctx;
-  zmq::socket_t sock(ctx, zmq::socket_type::pair);
+  zmq::socket_t sock(ctx, zmq::socket_type::xsub);
   try {
-    sock.bind(socket);
+    sock.set(zmq::sockopt::linger, 0);
+    sock.connect(socket);
+    auto header = std::string(&ZMQ_SUBSCRIBE_HEADER, 1);
+    auto message = zmq::message_t{header.c_str(), header.size()};
+    sock.send(message, zmq::send_flags::none);
   } catch (std::exception e) {
-    std::cout << e.what() << std::endl;
+    std::cout << "Failed to connect to ZMQ socket " << socket << ": " << e.what() << std::endl;
   }
 
+  zmq::pollitem_t items[] = {
+    {sock.handle(), 0, ZMQ_POLLIN, 0},
+  };
   zmq::message_t message;
   while (runFlag.load()) {
-    std::cout << "Receiving " << std::endl;
-    sock.recv(message);
-    char *data = message.data<char>();
-    std::cout << data << std::endl;
-    co_yield {0, 0, 0, 0};
+    zmq::poll(&items[0], sizeof(items) / sizeof(items[0]));
+    if (!(items[0].revents & ZMQ_POLLIN)) { // No data to receive
+      continue;
+    } 
+
+    auto rc = sock.recv(message);
+    if (sock.get(zmq::sockopt::rcvmore) != 0) {
+      const std::string header = message.to_string();
+      if (header == "V") {
+        auto rc2 = sock.recv(message);
+        DvsEvent *data = message.data<DvsEvent>();
+        co_yield {data->timestamp, data->x, data->y, data->polarity};
+      }
+    }
   }
   sock.close();
 }
--- src/input/zmq.hpp
+++ src/input/zmq.hpp
@@ -1,4 +1,13 @@
 #include "../aer.hpp"
 #include "../generator.hpp"
 
+struct DvsEvent {
+  unsigned int polarity : 1;
+  unsigned int y : 8;
+  unsigned int x : 8;
+  unsigned int timestamp : 32;
+} __attribute__((packed));
+
+constexpr char ZMQ_SUBSCRIBE_HEADER = 1;
+
 Generator<AER::Event> open_zmq(const std::string socket, std::atomic<bool>& runFlag);
