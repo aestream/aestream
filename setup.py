3141725469b40131f52bc3919d143d7926fd1a7d
Jens E. Pedersen <jensegholm@protonmail.com>
2023-07-17 13:11:42 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except:
 # Setuptools entrypoint
 setup(
     name="aestream",
-    version="0.5.1",
+    version="0.6.0",
     author="Jens E. Pedersen, Christian Pehle",
     author_email="jens@jepedersen.dk, christian.pehle@gmail.com",
     url="https://github.com/aestream/aestream",
