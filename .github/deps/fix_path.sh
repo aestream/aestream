4397a2255fd0e9141fb5352cc3110b4450eb11df
Jens E. Pedersen <jensegholm@protonmail.com>
2023-07-25 23:12:36 +0200

    Moved to pip as build backend to allow .so install

--- .github/deps/fix_path.sh
+++ .github/deps/fix_path.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-AESTREAM_PATH=$(find /opt -name 'aestream' -type d)
+AESTREAM_PATH=$(find /opt -name 'aestream' -type d | grep lib)
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${AESTREAM_PATH}"
