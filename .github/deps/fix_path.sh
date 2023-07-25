f5c722b99ceac178af30bfb68d6d40a9645a31a8
Jens E. Pedersen <jensegholm@protonmail.com>
2023-07-25 22:29:30 +0200

    Fixed repairwheel path

--- .github/deps/fix_path.sh
+++ .github/deps/fix_path.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-INSTALL_PATH=$(find _skbuild -name 'aestream' -type d | grep install)
+INSTALL_PATH=$(find /opt -name 'aestream' -type d)
 AESTREAM_PATH=$(readlink -f "$INSTALL_PATH")
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${AESTREAM_PATH}"
