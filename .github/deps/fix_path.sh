198ea5be9f518fbab9f4fbf667626d45061402d8
Jens E. Pedersen <jensegholm@protonmail.com>
2023-07-25 23:34:27 +0200

    Concatenated paths from multiple manylinux builds

--- .github/deps/fix_path.sh
+++ .github/deps/fix_path.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-AESTREAM_PATH=$(find /opt -name 'aestream' -type d | grep lib)
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${AESTREAM_PATH}"
+# Thanks to https://stackoverflow.com/a/17841619
+function join_by {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
+AESTREAM_PATHS=$(find /opt -name 'aestream' -type d | grep lib)
+INSTALL_PATHS=`join_by ':' $AESTREAM_PATHS`
+echo "Setting LD_LIBRARY_PATH to ${INSTALL_PATHS}"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${INSTALL_PATHS}"
